2777b65e130c2bf4b5b6f9a92e293deb2fb9836b
Karl <jocelinc60@outlook.com>
2020-03-23 10:39:20 -0400

    finish early version

---
+++ Form.py
@@ -0,0 +1,16 @@
+import helium
+
+def fill_form(username, password):
+    start_firefox(
+        'https://docs.google.com/forms/d/e/1FAIpQLScSW4ZPkJZN_i84epira40_ndrlsMz-avMcXoGUz7JYsJY2Gg/viewform?vc=0&c=0&w=1',
+    headless=True)
+    write(username, into='Email')
+    click('Next')
+    write(password, into='Enter')
+    click('Next')
+    click('Yes')
+    write('None', into="Questions")
+    click('Submit')
+    kill_browser()
+
+fill_form(#yourusername@stuy.edu, password)
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # attendenceForm
 basic python script
+
+moddify the file Form.py at then end and switching out the comments in fill_form() with your username and password in quotes.
+
+or 
+
+python -i Form.py
+> fill_form(username, pass)
